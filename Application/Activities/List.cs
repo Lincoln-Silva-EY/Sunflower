3d6d08563a18a6efacce23e8897afcec066a1172
Lincoln Andrade Silva <Lincoln.Silva1@br.ey.com>
2022-03-04 20:13:47 -0300

    CancellationToken

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -18,14 +19,32 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
 
+            private readonly ILogger<List> _logger;
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            public Handler(DataContext context, ILogger<List> logger)
             {
+                _logger = logger;
                 _context = context;
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
+
+                try
+                {
+                    for (var i = 0; i < 5; i++)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(1000, cancellationToken);
+                        _logger.LogInformation($"Task {i} foi finalizada");
+                    }
+                }
+
+                catch (Exception ex) when (ex is TaskCanceledException)
+                {
+                    _logger.LogInformation("A Task foi cancelada");
+                }
+
                 return await _context.Activities.ToListAsync();
             }
         }
