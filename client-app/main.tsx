6cb5b39825fdde73baaff33bb476337c4234cfa6
Lincoln Andrade Silva <Lincoln.Silva1@br.ey.com>
2022-03-04 22:04:40 -0300

    architecture

--- client-app/main.tsx
+++ client-app/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './app/layout/style.css';
-import App from './app/layout/App';
+import './src/app/layout/style.css';
+import App from './src/app/layout/App';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Header } from 'semantic-ui-react';
 import { List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/activities').then(response => {
+    axios.get<Activity[]>('http://localhost:8000/api/activities').then(response => {
       setActivities(response.data);
     })
   }, [])
 
   return (
     <div>
-      <Header as='h2' icon='sun' color='yellow' content='Sunflower'/>
+      <NavBar />
 
       <List>
-        {activities.map((activity: any) => (
+        {activities.map(activity => (
           <List.Item key={activity.id}>
             <br></br>
             {activity.title} <br></br>
