1cdcdc7c93122b0a38199d198fdb1febd67474dd
Lincoln Andrade Silva <Lincoln.Silva1@br.ey.com>
2022-03-07 15:27:46 -0300

    delete-on-server

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -7,9 +7,10 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit, deleteActivity }: Props) {
+export default function ActivityForm({ activity: selectedActivity, closeForm, createOrEdit, deleteActivity, submitting }: Props) {
 
     const initialState = selectedActivity ?? {
         id: '',
@@ -42,7 +43,7 @@ export default function ActivityForm({ activity: selectedActivity, closeForm, cr
                 <Form.Input placeholder='Date' type="date" value={activity.date} name='date' onChange={handleInputChange} />
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
-                <Button floated='right' positive type='submit' content='Submit' />
+                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
