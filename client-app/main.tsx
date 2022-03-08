ae458b4dc9b7a1479f565bf230965a2456b4cae0
Lincoln Andrade Silva <Lincoln.Silva1@br.ey.com>
2022-03-08 15:37:45 -0300

    Routing

--- client-app/main.tsx
+++ client-app/main.tsx
@@ -4,10 +4,13 @@ import './src/app/layout/style.css';
 import App from './src/app/layout/App';
 import 'semantic-ui-css/semantic.min.css';
 import { store, StoreContext } from './src/app/stores/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StoreContext.Provider>,
   document.getElementById('root')
 );
