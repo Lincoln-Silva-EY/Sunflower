762e2adf7a9c3f966851272a6d60f983a5daa752
Lincoln Andrade Silva <Lincoln.Silva1@br.ey.com>
2022-03-10 09:57:18 -0300

    AppUser-AccountControllerLogin

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -16,7 +18,9 @@ namespace API
             try
             {
                 var context = services.GetRequiredService<DataContext>();
-                await context.Database.MigrateAsync();    
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context, userManager);
             }
             catch (Exception ex)
             {
