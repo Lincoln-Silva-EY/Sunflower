034348befd1bf0690908399d18d49dca3ce0d157
Lincoln Andrade Silva <Lincoln.Silva1@br.ey.com>
2022-03-11 15:57:02 -0300

    Relationships

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,6 +11,23 @@ namespace Persistence
 
         }
 
-        public DbSet<Activity> Activities {get; set; } 
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendes { get; set; }
+
+        protected override async void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
+            
+            builder.Entity<ActivityAttendee>()
+            .HasOne(u => u.AppUser)
+            .WithMany(a => a.Activities)
+            .HasForeignKey(aa => aa.AppUserId);
+
+             builder.Entity<ActivityAttendee>()
+            .HasOne(u => u.Activity)
+            .WithMany(a => a.Attendees)
+            .HasForeignKey(aa => aa.ActivityId);
+        }
     }
 }
