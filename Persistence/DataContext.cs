9241b05dee83e9a6daa9a31ab759b52740efb7b6
Lincoln Andrade Silva <Lincoln.Silva1@br.ey.com>
2022-03-14 16:11:05 -0300

    API-Image

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -8,26 +8,25 @@ namespace Persistence
     {
         public DataContext(DbContextOptions options) : base(options)
         {
-
         }
 
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> ActivityAttendes { get; set; }
-
+        public DbSet<Photo> Photos { get; set; }
         protected override async void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
-            
+
             builder.Entity<ActivityAttendee>()
             .HasOne(u => u.AppUser)
             .WithMany(a => a.Activities)
             .HasForeignKey(aa => aa.AppUserId);
 
-             builder.Entity<ActivityAttendee>()
-            .HasOne(u => u.Activity)
-            .WithMany(a => a.Attendees)
-            .HasForeignKey(aa => aa.ActivityId);
+            builder.Entity<ActivityAttendee>()
+           .HasOne(u => u.Activity)
+           .WithMany(a => a.Attendees)
+           .HasForeignKey(aa => aa.ActivityId);
         }
     }
 }
