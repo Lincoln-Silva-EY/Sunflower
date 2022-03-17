fb84c7079f8604d294dcdfdf04f1fca22adb7935
Lincoln Andrade Silva <Lincoln.Silva1@br.ey.com>
2022-03-17 08:52:44 -0300

    ScrollTop

--- client-app/main.tsx
+++ client-app/main.tsx
@@ -9,12 +9,14 @@ import 'semantic-ui-css/semantic.min.css';
 import { store, StoreContext } from './src/app/stores/store';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import ScrollTop from './src/app/layout/ScrollTop';
 
 export const history = createBrowserHistory();
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
     <Router history={history}>
+      <ScrollTop />
       <App />
     </Router>
   </StoreContext.Provider>,
